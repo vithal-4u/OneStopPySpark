14f86e33be507acbb56691f7f131130733af08b6
Ashok <k.ashokachary@gmail.com>
2020-04-20 22:58:40 +0530

    PySpark WordCount example

---
+++ src/com/pyspark/poc/wc/WordCount.py
@@ -0,0 +1,22 @@
+'''
+Created on 20-Apr-2020
+
+@author: kasho
+'''
+
+from pyspark import SparkContext, SparkConf
+
+if __name__ == "__main__":
+    # create Spark context with necessary configuration
+    sc = SparkContext("local", "PySpark Word Count Exmaple")
+
+    # read data from text file and split each line into words
+    words = sc.textFile("D:/Study_Document/pycharm-workspace/OneStopPySpark/resources/wordcountData.txt").flatMap(lambda line: line.split(" "))
+    
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+    output = wordCounts.collect()
+
+    for (word, count) in output:
+        print("%s: %i" % (word, count))
+    print("Execution Completed")
