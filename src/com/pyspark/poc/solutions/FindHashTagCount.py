b1a31d9d39743a7e43e3cfa1c4bc6ee3f1b88187
Ashok <k.ashokachary@gmail.com>
2020-09-02 23:04:59 +0530

    Adding new solutions

---
+++ src/com/pyspark/poc/solutions/FindHashTagCount.py
@@ -0,0 +1,57 @@
+'''
+Created on 18-July-2020
+Requirement:
+    Read Json and List out the count of #TAG word with user name of it.
+
+@author: Ashok Kumar
+'''
+
+from com.pyspark.poc.utils.BaseConfUtils import BaseConfUtils
+from pyspark.sql.functions import explode
+
+conf = BaseConfUtils()
+spark = conf.createSparkSession("HashTagCount")
+
+def iteration(user):
+    print(user)
+
+def fetchRowData(row):
+    user_name=row["USER_NAME"]
+    text=row["TEXT"]
+    words=text.split(" ")
+    return (user_name,words)
+
+def filterMyHashTag(row):
+    l = list()
+    key=row[0]
+    for word in row[1]:
+        if "#" in word:
+            l.append(word)
+
+    rowval=(key,l)
+    return rowval
+if __name__ == "__main__":
+    #userTweets = spark.read.json("D:/Study_Document/GIT/OneStopPySpark/resources/Epam.json").rdd
+    #userTweets.foreach(iteration)
+    #userTweets.flatMap(lambda x: iteration(x))
+
+    userTweets = spark.read.json("D:/Study_Document/GIT/OneStopPySpark/resources/Epam.json")
+    userTweets.printSchema()
+
+    #nobelRDD = userTweets.select('USER_NAME', explode(userTweets['TEXT'])).rdd
+    #nobelRDD.collect()
+    userTweetsPojo = userTweets.createTempView("UserTweets")
+    userTextRDD = spark.sql("SELECT USER_NAME,TEXT FROM UserTweets").rdd
+    userSplitTextRDD=userTextRDD.map(lambda row : fetchRowData(row))
+    userHashTagTextRDD=userSplitTextRDD.flatMap(lambda row: filterMyHashTag(row))
+
+    #fitlerVal=key.filter(lambda row : filterMyHashTag(row))
+    userHashTagTextRDD.foreach(print)
+
+    df = userHashTagTextRDD.toDF()
+    print(df)
+    #print(fitlerVal.take(10))
+    #print(teenagerNamesDF.take(10))
+    #print(teenagerNamesDF['USER_NAME'])
+    #teenagerNamesDF.show()
+
---
+++ src/com/pyspark/poc/utils/CommanUtils.py
@@ -0,0 +1,11 @@
+import re
+
+class CommanUtils():
+
+    #####
+    # Below  COMMA_DELIMITER help not split "," value which is have inside a String ex: "Sao Filipe, Fogo Island"
+    # 5675,"Sao Filipe Airport","Sao Filipe, Fogo Island","Cape Verde","SFL","GVSF",14.885,-24.48,617,-1,"U","Atlantic/Cape_Verde"
+    # 5674,"Praia International Airport","Praia, Santiago Island","Cape Verde","RAI","GVNP",14.9245,-23.4935,230,-1,"U","Atlantic/Cape_Verde"
+    #
+    #####
+    COMMA_DELIMITER = re.compile(''',(?=(?:[^"]*"[^"]*")*[^"]*$)''')
